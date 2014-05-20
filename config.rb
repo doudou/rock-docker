e64c17129ab9023f9d7b69c1d9b30a496ced6ad1
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-05-20 21:57:51 +0200

    update main config file and config.rb to new version of autoproj-docker

--- config.rb
+++ config.rb
@@ -1,14 +1,43 @@
 username 'rock'
-debian = image 'debian' do
+image 'debian' do
+    config 'dev_mode' do
+        add true
+        add false
+    end
     config 'ruby' do
-        add 'ruby1.9.1', '/usr/bin/ruby1.9.1'
-        add 'ruby2.0', '/usr/bin/ruby2.0'
-        add 'ruby2.1', '/usr/bin/ruby2.1'
+        add ['ruby1.9.1', '/usr/bin/ruby1.9.1']
+        add ['ruby2.0', '/usr/bin/ruby2.0']
+        add ['ruby2.1', '/usr/bin/ruby2.1']
     end
     tag 'unstable'
+    tag 'testing'
 end
 
+image 'ubuntu' do
+    config 'dev_mode' do
+        add true
+        add false
+    end
+
+    config 'ruby' do
+        add ['ruby1.9.1', '/usr/bin/ruby1.9.1']
+    end
+
+    tag '14.04'
+end
+
+image 'opensuse' do
+    docker_image 'andtecheu/opensuse_13.1'
+    config 'dev_mode' do
+        add true
+    end
+    config 'ruby' do
+        add ['ruby1.9.1', '/usr/bin/ruby1.9.1']
+    end
+end
+
+
 build 'bootstrap'
-build 'update', :on => debian.
-    with('tag' => 'unstable',
-         'ruby' => ['ruby1.9.1', '/usr/bin/ruby1.9.1'])
+# build 'update', :on => debian.
+#     with('tag' => 'unstable',
+#          'ruby' => ['ruby1.9.1', '/usr/bin/ruby1.9.1'])
