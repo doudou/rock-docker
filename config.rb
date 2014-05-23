31f8561f12dc342ba06091c5f01f9328f7d39595
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-05-23 21:01:54 +0200

    fix arch, add fedora and all ubuntu versions since 12.04

--- config.rb
+++ config.rb
@@ -25,15 +25,38 @@ image 'ubuntu' do
         add false
     end
 
-    config 'ruby' do
-        add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
-    end
 
     config 'package_manager' do
         add ['apt-get update', 'apt-get install -y %s'], metadata: nil
     end
 
-    tag '14.04'
+    tag '12.04' do
+        config 'ruby' do
+            add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
+        end
+    end
+    tag '12.10' do
+        config 'ruby' do
+            add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
+        end
+    end
+    tag '13.04' do
+        config 'ruby' do
+            add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
+        end
+    end
+    tag '13.10' do
+        config 'ruby' do
+            add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
+            add ['ruby2.0', '/usr/bin/ruby2.0'], metadata: '2.0'
+        end
+    end
+    tag '14.04' do
+        config 'ruby' do
+            add ['ruby1.9.1', '/usr/bin/ruby1.9.1'], metadata: '1.9'
+            add ['ruby2.0', '/usr/bin/ruby2.0'], metadata: '2.0'
+        end
+    end
 end
 
 image 'opensuse' do
@@ -60,14 +83,25 @@ image 'arch' do
         add true
     end
     config 'ruby' do
-        add ['ruby19', '/usr/bin/ruby1.9'], metadata: '1.9'
-        add ['ruby20', '/usr/bin/ruby2.0'], metadata: '2.0'
+        add ['ruby', '/usr/bin/ruby'], metadata: '2.1'
     end
     config 'package_manager' do
-        add ['true', 'zypper --non-interactive install %s'], metadata: nil
+        add ['true', 'pacman -Sy --noconfirm %s'], metadata: nil
     end
 end
 
+image 'fedora' do
+    config 'dev_mode' do
+        add true
+    end
+    config 'ruby' do
+        add ['ruby', '/usr/bin/ruby'], metadata: '2.0'
+    end
+    config 'package_manager' do
+        add ['true', 'yum install -y %s'], metadata: nil
+    end
+    tag '20'
+end
 
 build 'bootstrap' do |image|
     image.metadata['dev_mode'] || (image.metadata['ruby'] == '1.9')
